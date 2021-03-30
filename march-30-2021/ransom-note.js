acdc582ac6847dab5b3224098e76f8133da51815
rsamhollyer <rsamhollyer@gmail.com>
2021-03-30 14:15:22 -0500

    updated ransom note

--- march-30-2021/ransom-note.js
+++ march-30-2021/ransom-note.js
@@ -1,73 +1,96 @@
-function checkMagazine(magazine, note) {
-  //Edge Cases
-  if (note.length > magazine.length) {
-    console.log(`No`);
-    return;
-  }
+//https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 
-  if (note.length === 0) {
-    console.log("No");
-    return;
-  }
-  ///
-  let noteObj = noteCache(note);
-  let startPoint = 0;
-  let endPoint = magazine.length - 1;
+// function checkMagazine(magazine, note) {
+//   //Edge Cases
+//   if (note.length > magazine.length) {
+//     console.log(`No`);
+//     return;
+//   }
 
-  let keySet = new Set(Object.keys(noteObj));
+//   if (note.length === 0) {
+//     console.log("No");
+//     return;
+//   }
+//   ///
+//   let noteObj = noteCache(note);
+//   let startPoint = 0;
+//   let endPoint = magazine.length - 1;
 
-  while (endPoint >= startPoint) {
-    if (startPoint === endPoint && noteObj[magazine[startPoint]] > 0) {
-      noteObj[magazine[startPoint]]--;
-      if (noteObj[magazine[startPoint]] === 0) {
-        keySet.delete(magazine[startPoint]);
-      }
-    }
+//   let keySet = new Set(Object.keys(noteObj));
 
-    if (noteObj[magazine[startPoint]] > 0) {
-      noteObj[magazine[startPoint]]--;
-      if (noteObj[magazine[startPoint]] === 0) {
-        keySet.delete(magazine[startPoint]);
-      }
-    }
-    if (noteObj[magazine[endPoint]] > 0) {
-      noteObj[magazine[endPoint]]--;
-      if (noteObj[magazine[endPoint]] === 0) {
-        keySet.delete(magazine[endPoint]);
-      }
-    }
+//   while (endPoint >= startPoint) {
+//     if (startPoint === endPoint && noteObj[magazine[startPoint]] > 0) {
+//       noteObj[magazine[startPoint]]--;
+//       if (noteObj[magazine[startPoint]] === 0) {
+//         keySet.delete(magazine[startPoint]);
+//       }
+//     }
 
-    if (keySet.size === 0) {
-      console.log("Yes");
-      return;
-    }
-    startPoint++;
-    endPoint--;
-  }
-  console.log("No");
-}
+//     if (noteObj[magazine[startPoint]] > 0) {
+//       noteObj[magazine[startPoint]]--;
+//       if (noteObj[magazine[startPoint]] === 0) {
+//         keySet.delete(magazine[startPoint]);
+//       }
+//     }
+//     if (noteObj[magazine[endPoint]] > 0) {
+//       noteObj[magazine[endPoint]]--;
+//       if (noteObj[magazine[endPoint]] === 0) {
+//         keySet.delete(magazine[endPoint]);
+//       }
+//     }
 
-function noteCache(str) {
-  const noteObj = {};
-  let startPoint = 0;
-  let endPoint = str.length - 1;
+//     if (keySet.size === 0) {
+//       console.log("Yes");
+//       return;
+//     }
+//     startPoint++;
+//     endPoint--;
+//   }
+//   console.log("No");
+// }
 
-  while (endPoint >= startPoint) {
-    if (startPoint === endPoint) {
-      noteObj[str[startPoint]] = (noteObj[str[startPoint]] || 0) + 1;
-    } else {
-      noteObj[str[startPoint]] = (noteObj[str[startPoint]] || 0) + 1;
-      noteObj[str[endPoint]] = (noteObj[str[endPoint]] || 0) + 1;
-    }
-    startPoint++;
-    endPoint--;
-  }
+// function noteCache(str) {
+//   const noteObj = {};
+//   let startPoint = 0;
+//   let endPoint = str.length - 1;
 
-  return noteObj;
-}
+//   while (endPoint >= startPoint) {
+//     if (startPoint === endPoint) {
+//       noteObj[str[startPoint]] = (noteObj[str[startPoint]] || 0) + 1;
+//     } else {
+//       noteObj[str[startPoint]] = (noteObj[str[startPoint]] || 0) + 1;
+//       noteObj[str[endPoint]] = (noteObj[str[endPoint]] || 0) + 1;
+//     }
+//     startPoint++;
+//     endPoint--;
+//   }
+
+//   return noteObj;
+// }
 
 let array1 = "give me one grand today night".split(" ");
 
 let array2 = "give one grand today".split(" ");
 
 checkMagazine(array1, array2);
+
+// MORE ELEGANT SOLUTION:
+
+function checkMagazine(magazine, note) {
+  if (note.length > magazine.length || note.length < 1) {
+    console.log("No");
+    return;
+  }
+  let result = "Yes";
+  const magazineMap = {};
+  const noteMap = {};
+  for (let word of magazine) {
+    magazineMap[word] = (magazineMap[word] || 0) + 1;
+  }
+  for (let word of note) {
+    noteMap[word] = (noteMap[word] || 0) + 1;
+    if (!magazineMap[word] || noteMap[word] > magazineMap[word]) result = "No";
+  }
+
+  console.log(result);
+}
