b7112049be885c82bef3afde5973ea5685ec705f
rsamhollyer <rsamhollyer@gmail.com>
2021-03-31 15:05:59 -0500

    subarray with sliding window

--- march-31-2021/subarray-division.js
+++ march-31-2021/subarray-division.js
@@ -1,18 +1,39 @@
 // https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+// function birthday(s, d, m) {
+//   let counter = 0;
+
+//   for (let i = 0; i < s.length - m + 1; i++) {
+//     let sum = s.slice(i, i + m).reduce((acc, cur) => acc + cur, 0);
+//     if (sum === d) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+
+let s = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1];
+let d = 18;
+let m = 7;
+
+// MORE EFFICIENT SLIDING WINDOW TECHNIQUE
+
 function birthday(s, d, m) {
   let counter = 0;
+  let sum = 0;
 
-  for (let i = 0; i < s.length - m + 1; i++) {
-    let sum = s.slice(i, i + m).reduce((acc, cur) => acc + cur, 0);
-    if (sum === d) {
-      counter++;
+  for (let i = 0, j = 0; i < s.length; i++) {
+    sum += s[i];
+
+    if (i + 1 >= m) {
+      if (sum === d) {
+        counter++;
+      }
+
+      sum -= s[j];
+      j++;
     }
   }
   return counter;
 }
-
-let s = [1, 2, 1, 3, 2];
-let d = 3;
-let m = 2;
 
 console.log(birthday(s, d, m));
